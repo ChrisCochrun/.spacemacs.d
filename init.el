b7e4d1440728ada269213a6b24daa1f42e7629d1
Chris Cochrun <chris@tfcconnection.org>
2020-05-07 06:24:28 -0500

    moving notes to org folder in home dir so syncing can be based on that location

--- init.el
+++ init.el
@@ -353,7 +353,7 @@ you should place your code here."
   (spacemacs/set-leader-keys "oi" 'helm-org-agenda-files-headings)
 
   ;; Set org agenda folder to all org files in nextcloud/notes
-  (setq org-agenda-files '("~/storage/Nextcloud/notes/"))
+  (setq org-agenda-files '("~/org/"))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
@@ -617,7 +617,7 @@ static char *gnus-pointer[] = {
  '(org-roam-buffer-width 0.22)
  '(org-roam-capture-after-find-file-hook nil)
  '(org-roam-completion-system (quote helm))
- '(org-roam-directory "/home/chris/storage/Nextcloud/notes/" nil nil "Customized with use-package org-roam")
+ '(org-roam-directory "~/org/" nil nil "Customized with use-package org-roam")
  '(package-selected-packages
    (quote
     (ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hydra lv hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile projectile pkg-info epl helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
